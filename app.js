31fa2a76e57b599670937ed1c884561517c6cf91
Onlyfans1seller <mishagolub301@gmail.com>
2026-03-06 20:49:03 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-import { connectEVM } from "./evm.js"
+import { connectEVM } from "./wallets.js"
 import { connectPhantom } from "./phantom.js"
 
-const modal = document.getElementById("walletModal")
 const btn = document.getElementById("connectBtn")
+const modal = document.getElementById("walletModal")
 
 btn.onclick = () => {
- modal.style.display = "flex"
+modal.style.display="flex"
 }
 
-document.getElementById("metamask").onclick = connectEVM
-document.getElementById("trust").onclick = connectEVM
-document.getElementById("coinbase").onclick = connectEVM
-document.getElementById("rainbow").onclick = connectEVM
-document.getElementById("phantom").onclick = connectPhantom
+document.getElementById("metamask").onclick=connectEVM
+document.getElementById("trust").onclick=connectEVM
+document.getElementById("coinbase").onclick=connectEVM
+document.getElementById("rainbow").onclick=connectEVM
+document.getElementById("phantom").onclick=connectPhantom
